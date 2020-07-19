70ba1589ac3217a2fd89ca5e13febc7022134909
Jachiike <jachiike.madubuko@gmail.com>
2020-07-18 21:01:10 -0500

    all sections created

--- content.py
+++ content.py
@@ -8,79 +8,82 @@ pages = ("🐍 for 👩🏿‍💻🧙🏿‍♂️",
          )
 page_data = [
     {
-    'bg':'primary',
-    'subtitle':'Python for Data Science',
-    'description':'Python for Data Science',
-    'sections':[
-        'Basics',
-        'Data Structures',
-        'Flow and Functions',
-        'NumPy',
-        'Pandas'
-    ]
+        'bg': 'primary',
+        'subtitle': 'Python for Data Science',
+        'description': 'Python for Data Science',
+        'sections': {
+            'Data Structures': "Lists, Tuples, Sets & Dictionaries",
+            # 'Data Structures': "I. Lists are mutable sequences of objects. \n II. Tuples are immutable sequences of objects. \n III. Sets are collections of unique objects. \n IV. Dictionaries are collections of key-value pairs.",
+            'Flow and Functions': "IF, FOR, List comprehensions, Functions",
+            'NumPy': "encore",
+            'Pandas': "broads in Atlanta"
+        }
     },
     {
-    'bg':'secondary',
-    'subtitle':'How the world works',
-    'description':'How the world works',
-    'sections':[
-        'Fake It Til You Make It',
-        'Mean Models & Linear Regression',
-        'Polynomial Regression & Model Parameters',
-        'Decision Trees & Model Tuning',
-        'Unseen Data & The Search for Empirical Truth',
-    ]
+        'bg': 'secondary',
+        'subtitle': 'How the world works',
+        'description': 'How the world works',
+        'sections': {
+            'Fake It Til You Make It': "nah",
+            'Mean Models & Linear Regression': "mean good",
+            'Polynomial Regression & Model Parameters': "poly",
+            'Decision Trees & Model Tuning': "trees",
+            'Unseen Data & The Search for Empirical Truth': "da truff",
+        }
     },
     {
-    'bg':'info',
-    'subtitle':'Real-Estate Tycoon',
-    'description':'Real-Estate Tycoon',
-    'sections': [
-            'Exploratory Analysis',
-            'Data Cleaning',
-            'Feature Engineering',
-            'Regression Model Selection',
-            'Model Training',
-            ]
+        'bg': 'info',
+        'subtitle': 'Real-Estate Tycoon',
+        'description': 'Real-Estate Tycoon',
+        'sections': {
+            'Exploratory Analysis': "explore",
+            'Data Cleaning': 'Data Cleaning',
+            'Feature Engineering': 'Feature Engineering',
+            'Regression Model Selection': 'Regression Model Selection',
+            'Model Training': 'Model Training',
+        }
     },
     {
-    'bg':'success',
-    'subtitle':'Know your people',
-    'description':'Know your people, know your power',
-    'sections': [
-            'Exploratory Analysis 2',
-            'ABT',
-            'Classification Model Selection',
-            'Model Evaluation',
-            'Production Prediction',
-        ]
+        'bg': 'success',
+        'subtitle': 'Know your people',
+        'description': 'Know your people, know your power',
+        'sections': {
+            'Exploratory Analysis 2': 'Exploratory Analysis 2',
+            'ABT': 'ABT',
+            'Classification Model Selection': 'Classification Model Selection',
+            'Model Evaluation': 'Model Evaluation',
+            'Production Prediction': 'Production Prediction',
+    }
     },
     {
-    'bg':'danger',
-    'subtitle':'Super swaggy',
-    'description':'Super swaggy',
-    'sections': [
-            'Data Wrangling',
-            'Dimensionality Reduction',
-            'PCA',
-            'Clustering Model Selection',
-            'Cluster Analysis',
-        ]
+        'bg': 'danger',
+        'subtitle': 'Super swaggy',
+        'description': 'Super swaggy',
+        'sections': {
+            'Data Wrangling': 'Data Wrangling',
+            'Dimensionality Reduction': 'Dimensionality Reduction',
+            'PCA': 'PCA',
+            'Clustering Model Selection': 'Clustering Model Selection',
+            'Cluster Analysis': 'Cluster Analysis',
+        }
     },
     {
-    'bg':'warning',
-    'subtitle':'To the stars',
-    'description':'To the stars',
-    'sections': [
-            'To The Stars',
-            'The Mission',
-            'The Launch',
-            'The Voyage',
-            'The Arrival',
-            'The New Planet'
-        ]},
+        'bg': 'warning',
+        'subtitle': 'To the stars',
+        'description': 'To the stars',
+        'sections': {
+            'To The Stars': 'To The Stars',
+            'The Mission': 'The Mission',
+            'The Launch': 'The Launch',
+            'The Voyage': 'The Voyage',
+            'The Arrival': 'The Arrival',
+            'The New Planet': 'The New Planet'
+        }
+    },
 ]
 page_render = dict(zip(list(pages), page_data))
+
+
 def write(text):
     if "<" in text:
         lit.write(text, unsafe_allow_html=True)
--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import streamlit as lit
 
 from bootstrap import *
 from content import *
+from sections import *
 from models import DataView
+
+
 
 def main():
     page = lit.sidebar.radio("Page",
@@ -22,22 +25,10 @@ def main():
     }
     write(jumbo.format_map(jumbo_layout))
     section = lit.sidebar.radio("Section",
-                                    page_render[page]['sections']
-                                    )
-    write(card.format(f'{page} - {section}',page,section))
-    if page == pages[0]:
-        data = pd.DataFrame([{'id': i, 'name': str(i)+section} for i in range(100)])
-        lit.write(data)
-    if page == pages[1]:
-        write(card.format(f'{page} - {section}',page,section))
-    if page == pages[2]:
-        write(card.format(f'{page+page} - {section}',page,section))
-    if page == pages[3]:
-        write(card.format(f'{section} ++ {page}',page,section))
-    if page == pages[4]:
-        write(card.format(f'{page} ~~ {section}',page,section))
-    if page == pages[5]:
-        write(card.format(f'{page} __ {section}',page,section))
+                                list(page_render[page]['sections'].keys())
+                                )
+    write(card.format(f'{page} - {section}', section, page_render[page]['sections'][section]))
+    section_render(section, lit)
     # d = DataView("d", lit)
     # t = DataView("t", lit)
     # d.render()
